0f0595acd702795a5957111c37e715d7eda399ed
Nikola <rebracanikola@gmail.com>
2018-12-22 13:27:39 +0100

    Enabled vehicle update.

--- AIrtickets/src/main/java/airtickets/controller/rentacar/VehicleController.java
+++ AIrtickets/src/main/java/airtickets/controller/rentacar/VehicleController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,4 +38,8 @@ public class VehicleController {
 			return vehicleService.addVehicle(vehicle);
 	}
 
+	@PutMapping("{id}/update")
+	public VehicleDTO updateVehicle(@RequestBody VehicleDTO vehicle) {
+		return vehicleService.addVehicle(vehicle);
+	}
 }
