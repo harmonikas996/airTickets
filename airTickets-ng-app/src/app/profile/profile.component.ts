ab2271efd9cc7ff583343d701794cddda5871066
Marko <marko.markovic561@gmail.com>
2019-02-08 23:01:01 +0100

    reservation

--- airTickets-ng-app/src/app/profile/profile.component.ts
+++ airTickets-ng-app/src/app/profile/profile.component.ts
@@ -1,3 +1,7 @@
+import { RoomReservation } from './../shared/model/hotel/room-reservation.model';
+import { RoomReservationService } from './../shared/services/hotel/room-reservation/room-reservation.service';
+import { CarReservation } from './../shared/model/rentacar/car-reservation';
+import { CarReservationService } from './../shared/services/rentacar/car-reservation.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +11,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  carReservations: CarReservation[];
+  carReservation: CarReservation;
+
+  roomReservations: RoomReservation[];
+  roomReservation: RoomReservation;
+
+  constructor(
+    private carReservationService: CarReservationService,
+    private roomReservationService: RoomReservationService
+  ) { }
 
   ngOnInit() {
+
+    this.getQuickReservation();
+    this.getQuickReservationRoom();
+  }
+
+  getQuickReservation(): void {
+    this.carReservationService.getCarReservations().subscribe(carReservations => this.carReservations = carReservations);
+  }
+
+  getQuickReservationRoom(): void {
+    this.roomReservationService.getRoomsRes().subscribe(roomReservations => this.roomReservations = roomReservations);
   }
 
 }
