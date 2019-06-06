a40d8151fdcf7be4f3d5d0a0609560cf5121dc06
Nikola <rebracanikola@gmail.com>
2019-06-06 15:27:01 +0200

    quick-fix of pom.xml

--- AIrtickets/src/main/java/airtickets/repo/aircompany/FlightRepository.java
+++ AIrtickets/src/main/java/airtickets/repo/aircompany/FlightRepository.java
@@ -1,6 +1,5 @@
 package airtickets.repo.aircompany;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
--- AIrtickets/src/main/java/airtickets/repo/rentacar/VehiclesRepository.java
+++ AIrtickets/src/main/java/airtickets/repo/rentacar/VehiclesRepository.java
@@ -14,7 +14,17 @@ public interface VehiclesRepository extends JpaRepository<Vehicle, Long> {
 	@Query(value="SELECT * FROM vehicle where rentacar_id = ?1", nativeQuery = true)
 	public List<Vehicle> findByRentACarId(long id);
 	
-	@Query(value="SELECT * FROM vehicle where id in (select vehicle.id from vehicle, rentacar where vehicle.rentacar_id=rentacar.id and rentacar.id = ?1 and vehicle.type = ?2 and vehicle.number_of_seats >= ?3 and vehicle.price_per_day >= ?4 and vehicle.price_per_day <= ?5 and vehicle.id not in (select vehicle.id from vehicle, car_reservation, rentacar where vehicle.id=car_reservation.vehicle_id and vehicle.rentacar_id=rentacar.id and rentacar.id = ?1 and car_reservation.date_from <= ?7 and car_reservation.date_to >= ?6))", nativeQuery = true)
+	@Query(value="SELECT * FROM vehicle where id in (select vehicle.id from vehicle, "
+			+ "rentacar where vehicle.rentacar_id=rentacar.id and rentacar.id = ?1 "
+			+ "and vehicle.type = ?2 and vehicle.number_of_seats >= ?3 "
+			+ "and vehicle.price_per_day >= ?4 "
+			+ "and vehicle.price_per_day <= ?5 "
+			+ "and vehicle.id "
+			+ "not in (select vehicle.id from vehicle, "
+			+ "car_reservation, rentacar "
+			+ "where vehicle.id=car_reservation.vehicle_id and vehicle.rentacar_id=rentacar.id "
+			+ "and rentacar.id = ?1 and car_reservation.date_from <= ?7 "
+			+ "and car_reservation.date_to >= ?6))", nativeQuery = true)
 	//@Query(value="SELECT * FROM vehicle where id in (select vehicle.id from vehicle, rentacar where vehicle.rentacar_id=rentacar.id and rentacar.id = ?1 and vehicle.type = ?2)", nativeQuery = true)
 	public List<Vehicle> searchVehicles(long rentacarId, int type, int passangers, double lowerPrice, double upperPrice, LocalDateTime from, LocalDateTime to);
 	@Query(value="SELECT * FROM vehicle where vehicle.rentacar_id = ?1 and id not in\r\n" + 
